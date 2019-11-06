b870c5383805d90846000d37cf4ce3f29354cd63
Daniel Williamson <danielwilliamson@Daniels-MacBook-Pro.local>
2019-11-06 11:34:09 +0000

    curious issues with source_python and 1 correction

--- TestingMoGP/GetPythonFunctions.py
+++ TestingMoGP/GetPythonFunctions.py
@@ -1,1 +1,7 @@
 from mogp_emulator.tests import benchmark_branin
+def f():
+  return benchmark_branin.__file__
+  
+
+
+
--- TestingMoGP/InteractingWithPython.R
+++ TestingMoGP/InteractingWithPython.R
@@ -6,21 +6,22 @@ library(reticulate)
 #exit
 #FROM THE MOGP_EMULATOR DIRECTORY
 #DW Run: setwd("/Users/danielwilliamson/Dropbox/BayesExeter/mogp_emulator")
-source_python("../ExeterUQ/TestingMoGP/GetPythonFunctions.py")
+source_python("../ExeterUQ/TestingMoGP/GetPythonFunctions.py") #this worked before making use of branin$ obsolete. Find out why it doesnt work now
+branin <- import("mogp_emulator.tests.benchmark_branin")
 n_emulators = 3
 n_simulations = 15
 n_testing = 10
-training <- generate_training_data(n_emulators,n_simulations)
+training <- branin$generate_training_data(n_emulators,n_simulations)
 names(training) <- c("inputs", "outputs", "em_params")
 #Note training$inputs is nxd (15x2) and training$outputs is mxn (3x15)
 
 #Fit the GP, first initialising then estimating parameters
-a_gp <- MultiOutputGP(training$inputs, training$outputs)
+a_gp <- branin$MultiOutputGP(training$inputs, training$outputs)
 GPlist <- a_gp$learn_hyperparameters()
 #Need to find out how these parameters are output and stored. 
 #Seems unclear as no method to return hyperparameters without refitting
 
-Validation <- generate_test_data(n_testing, training$em_params)
+Validation <- branin$generate_test_data(n_testing, training$em_params)
 names(Validation) <- c("Valid_inputs","Valid_outputs")
 
 #Make predictions
@@ -33,7 +34,7 @@ names(predictions) <- c("Expectation","Variance","Derivatives")
 #Try Save and reload
 a_gp$save_emulators(filename = "../ExeterUQ/TestingMoGP/FirstPythonEmulators")
 rm(a_gp)
-newGP <- MultiOutputGP("../ExeterUQ/TestingMoGP/FirstPythonEmulators.npz")
+newGP <- branin$MultiOutputGP("../ExeterUQ/TestingMoGP/FirstPythonEmulators.npz")
 predictions2 <- newGP$predict(Validation$Valid_inputs, do_deriv = FALSE, do_unc = TRUE)
 names(predictions2) <- c("Expectation","Variance","Derivatives")
 (Validation$Valid_outputs - predictions2$Expectation)/sqrt(predictions2$Variance)
